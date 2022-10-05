b9828bca3ab4482af044c400aad2952b487da468
Top dev <73906697+babystar0908@users.noreply.github.com>
2022-10-05 21:20:44 +0200

    update

--- src/pages/mint/mint.js
+++ src/pages/mint/mint.js
@@ -15,9 +15,17 @@ const Mint = () => {
   return (
     <Containertag sx={{ height: "100%" }}>
       <Container maxWidth="md" sx={{ background: "#e32426" }}>
-        {/* <Grid className="mintGrid" sx={{ background: "#a7a7a7", padding: "20px 15px 20px 15px", display: "flex", borderRadius: 0 }}> */}
-        <Connect />
-        {/* </Grid> */}
+        <Grid
+          className="mintGrid"
+          sx={{
+            background: "#a7a7a7",
+            padding: "20px 15px 20px 15px",
+            display: "flex",
+            borderRadius: 0,
+          }}
+        >
+          <Connect />
+        </Grid>
       </Container>
     </Containertag>
   );
