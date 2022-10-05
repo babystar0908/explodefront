532936fb1adf4a49a56e147c9aa7b37c6e615830
Top dev <73906697+babystar0908@users.noreply.github.com>
2022-10-05 21:17:19 +0200

    Update mint.js
    Co-Authored-By: Progressive Developer <72561743+DarkBear0121@users.noreply.github.com>
    Co-Authored-By: Vecnazmaga <103976145+vecnazmaga@users.noreply.github.com>

--- src/pages/mint/mint.js
+++ src/pages/mint/mint.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import Container from "@mui/material/Container"
-import Grid from "@mui/material/Grid"
-import { styled } from '@mui/material/styles';
+import React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
 import Connect from "./ModalCom";
 
 const Containertag = styled(Grid)(({ theme }) => ({
-    padding: theme.spacing(5),
-    display: "flex",
-    background: "#e32426",
-    justifyContent: "center", alignItems: "center"
+  padding: theme.spacing(5),
+  display: "flex",
+  background: "#e32426",
+  justifyContent: "center",
+  alignItems: "center",
 }));
 const Mint = () => {
-
-    return (
-        <Containertag sx={{ height: "100%" }}>
-            <Container maxWidth="md" sx={{ background: "#e32426" }}>
-                <Grid className="mintGrid" sx={{ background: "#a7a7a7", padding: "20px 15px 20px 15px", display: "flex", borderRadius: 0 }}>
-                    <Connect />
-                </Grid>
-            </Container>
-        </Containertag>
-
-    );
-}
+  return (
+    <Containertag sx={{ height: "100%" }}>
+      <Container maxWidth="md" sx={{ background: "#e32426" }}>
+        {/* <Grid className="mintGrid" sx={{ background: "#a7a7a7", padding: "20px 15px 20px 15px", display: "flex", borderRadius: 0 }}> */}
+        <Connect />
+        {/* </Grid> */}
+      </Container>
+    </Containertag>
+  );
+};
 export default Mint;
